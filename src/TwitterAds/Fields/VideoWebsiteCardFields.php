efe33328a39eb025e345726305fbb70497008408
Le_Tung <le.van.tung@aainc.co.jp>
2017-12-13 15:49:19 +0900

    support video website card

---
+++ src/TwitterAds/Fields/VideoWebsiteCardFields.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hborras\TwitterAdsSDK\TwitterAds\Fields;
+
+
+class VideoWebsiteCardFields
+{
+    const ID             = 'id';
+    const NAME           = 'name';
+    const TITLE          = 'title';
+    const VIDEO_ID       = 'video_id';
+    const CREATED_AT     = 'created_at';
+    const UPDATED_AT     = 'updated_at';
+    const DELETED        = 'deleted';
+    const PREVIEW_URL    = 'preview_url';
+    const WEBSITE_URL    = 'website_url';
+}
